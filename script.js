5a7ac53cc2a2b1379c3c4b2d6003916596790654
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-02-25 11:31:24 +0000

    html-division-services

---
+++ script.js
@@ -0,0 +1,30 @@
+// const dataForServices = [
+// 	{
+// 		id: 1,
+// 		image: pictureOne,
+// 		heading: "UX & UI",
+// 		paragraph:
+// 			"Designing interfaces that are intuitive, efficient, and enjoyable to use.",
+// 	},
+// 	{
+// 		id: 2,
+// 		image: pictureTwo,
+// 		heading: "Web & Mobile App",
+// 		paragraph:
+// 			"Transforming Ideas into exceptional web and mobile app experiences.",
+// 	},
+// 	{
+// 		id: 3,
+// 		image: pictureOne,
+// 		heading: "Design & Creative",
+// 		paragraph:
+// 			"Designing interfaces that are intuitive, efficient, and enjoyable to use.",
+// 	},
+// 	{
+// 		id: 4,
+// 		image: pictureOne,
+// 		heading: "Development",
+// 		paragraph:
+// 			"Bringing your vision to life with the latest technology and design trends.",
+// 	},
+// ];
